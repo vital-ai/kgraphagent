7764c8b1238777b85db424af800f89edfaf59f5e
Marc Hadfield <marc@vital.ai>
2024-08-21 19:24:07 -0400

    sync

--- kgraphagent/kgraphagent_manager.py
+++ kgraphagent/kgraphagent_manager.py
@@ -1,3 +1,17 @@
 
 class KGraphAgentManager:
     pass
+
+# manager class for agents utilizing knowledge graphs
+# queries can be used with kgraphservice and kgraphmemory
+
+# this library is agnostic with regard to libraries used to implement agents
+# such as langchain and llamaindex
+
+# such a library may use kgraphagent as a dependency in its implementation or
+# an agent implementation may use such a library and wrap the functionality in
+# this library
+
+# handle unpacking containers of kg message history
+# may use data definitions in kgraphservice, such as message part
+
--- kgraphagent/query/kgraphagent_query.py
+++ kgraphagent/query/kgraphagent_query.py
@@ -1,3 +1,17 @@
 
 class KGraphAgentQuery:
     pass
+
+# base class for queries
+
+# query functions meant to be wrapped in an LLM function call
+# for particular agent implementations using libraries such as langchain, llamaindex
+
+# queries may simply wrap existing query methods defined in kgservice like for kgframe
+# or may define queries using objects/functions from kgservice
+
+# query functions are meant to be applied to the underlying kgservice for a
+# graph/vector database query or applied to kgmemory for an in-memory graph/vector query
+
+
+
